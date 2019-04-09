02229957255fde1236c0337383a4b9e61763cb60
xarkenz <46010001+xarkenz@users.noreply.github.com>
2019-04-08 20:53:05 -0500

    Compatibility upgrades (new interpret function)

--- syntax.py
+++ syntax.py
@@ -1,10 +1,6 @@
-uadds_ = {}
-availadd_ = {}
-utypes_ = {}
-availtype_ = {}
-ufuncs_ = {}
-availfunc_ = {}
-mem = ({},{},{},{})
+_all_ = ({},{},{})
+_avail_ = ({},{},{})
+_loaded_ = ({},{},{})
 
 def isfloat(self):
     try:
@@ -15,70 +11,55 @@ def isfloat(self):
 class AddError(Exception): pass
 
 def cacheAdd(add):
-    mem[0][add.name] = add
+    _loaded_[0][add.name] = add
 def cacheType(typ):
-    mem[1][typ.name] = typ
+    _loaded_[0][typ.name] = typ
 def cacheFunc(func):
-    mem[2][func.name] = func
+    _loaded_[0][func.name] = func
+
 def getAdd(name):
     try:
-        ret = availadd_[name]
-        for i in ret.adds: availadd_[i.name] = i
-        for i in ret.types: availtype_[i.name] = i
-        for i in ret.funcs: availfunc_[i.name] = i
+        ret = _avail_[0][name]
+        for add in ret.adds: _avail_[0][add.name] = add
+        for typ in ret.types: _avail_[1][typ.name] = typ
+        for func in ret.funcs: _avail_[2][func.name] = func
         return ret
-    except KeyError:
-        raise AddError("Unable to locate addition '%s'" % name)
+    except KeyError: raise AddError("Unable to locate addition '%s'" % name)
 def getFunc(name):
     try:
-        return availfunc_[name]
-    except KeyError:
-        raise NameError("Function '%s' is not defined" % name)
+        return _avail_[2][name]
+    except KeyError: raise NameError("Function '%s' is not defined" % name)
+class uadd:
+    def __init__(self, name:str, adds, types, funcs, *, available:bool=False):
+        self.name, self.adds, self.types, self.funcs, self.avail = name, adds, types, funcs, available
+        _all_[0][self.name] = self
+        if self.avail: _avail_[0][self.name] = self
+    def __str__(self):
+        return self.name
+class utype:
+    def __init__(self, name:str, inherits, *, base:bool=False):
+        self.name, self.inherits, self.base = name, inherits, base
+        _all_[1][self.name] = self
+        #if self.base: _avail_[1][self.name] = self
+    def __str__(self):
+        return self.name
 class ufunc:
-    def __init__(self, name:str, function, avail:bool=False):
-        self.name = name
-        self.func = function
-        self.available = avail
-        if self.available: availfunc_[self.name] = self
+    def __init__(self, name:str, function):
+        self.name, self.func = name, function
+        _all_[2][self.name] = self
     def __call__(self, *args):
         return self.func(*args)
     def __str__(self):
         return self.name
-class utype:
-    def __init__(self, name:str, inherits:tuple, *, base:bool=False):
-        self.name = name
-        self.inherits = inherits
-        self.isbase = base
-class uadd:
-    def __init__(self, name:str, adds, types, funcs, *, avail:bool=False):
-        self.name = name
-        self.adds = adds
-        self.types = types
-        self.funcs = funcs
-        self.available = avail
-        uadds_[self.name] = self
-        if self.available: availadd_[self.name] = self
-        
-input_ = ufunc("input", input)
-print_ = ufunc("print", print)
-type_ = utype("type", [], base=True)
-str_ = utype("str", [])
-list_ = utype("list", [])
-note_ = utype("note", [])
-track_ = utype("track", [])
-tool_ = utype("tool", [])
-plugin_ = utype("plugin", [])
-Plugin_ = uadd("Plugin", [], [], [])
-NOTE_ = uadd("NOTE", [], [note_, track_], [])
-CONTRAST_ = uadd("CONTRAST", [], [tool_], [])
-UNION_ = uadd("UNION", [Plugin_, NOTE_, CONTRAST_], [type_, plugin_, list_, str_], [print_, input_], avail=True)
-def obj(otype, name, values):
+
+def obj(otype, name, values=[]):
     values = [str(i) for i in values]
     if len(values) > 0: return "<%s object '%s' with assigned values (%s)>" % (otype, name, ", ".join(values))
     else: return "<%s object '%s'>" % (otype, name)
 def inst(which, values): return "<%s instruction with parameters (%s)>" % (which, ", ".join(values))
 def char(which):
     if which == ",": return "<End of instruction>"
+    else: raise SyntaxError("Invalid syntax")
 def biobj(typ, data):
     if typ == str: return "<str %s>" % data
     elif typ == int: return "<int %s>" % data
@@ -87,12 +68,9 @@ def biobj(typ, data):
 def itemsBrackets(string):
     if not (string.startswith("[") and string.endswith("]")): raise SyntaxError("Unclosed bracket set")
     return [block(it) for it in string.replace("[","").replace("]","").split(";")]
-content=list(input().replace("\\n",",").split(","))
-output="<SOF>"
 
 def block(item):
-    item = item.replace(" ", "").split("//")[0]
-    
+    item = item.replace(" ","").split("//")[0]
     if item.startswith("$"):
         values = list(item.split("!"))
         del values[0]
@@ -113,14 +91,29 @@ def block(item):
     elif isfloat(item): return biobj(float, item)
     else: raise SyntaxError("Invalid syntax")
 
-delcount = 0
-for i in range(len(content)):
-    content[i-delcount] = content[i-delcount].split("#")[0]
-    if content[i-delcount].replace(" ","") == "": del content[i-delcount]; delcount += 1
-cmt = False
-for i, lineno in zip(content, range(len(content))):
-    output += block(i)
-    output += char(",")
-output += "<EOF>"
-print(output)
-print(mem)
+input_ = ufunc("input", input)
+print_ = ufunc("print", print)
+type_ = utype("type", [], base=True)
+str_ = utype("str", [])
+list_ = utype("list", [])
+NOTE_ = uadd("NOTE", [], [], [])
+CONTRAST_ = uadd("CONTRAST", [], [], [])
+UNION_ = uadd("UNION", [NOTE_, CONTRAST_], [type_, list_, str_], [print_, input_], available=True)
+
+def interpret(inp:str):
+    cont = list(inp.replace("\n",",").split(","))
+    out = "<SOF>"
+    delcount = 0
+    for i in range(len(cont)):
+        cont[i-delcount] = cont[i-delcount].split("#")[0]
+        if cont[i-delcount].replace(" ","") == "": del cont[i-delcount]; delcount += 1
+    cmt = False
+    for i, lineno in zip(cont, range(len(cont))):
+        out += block(i) + char(",")
+    out += "<EOF>"
+    return out
+
+print(interpret("""
+$UNION,$CONTRAST
+>>print["stuff"]
+"""))
